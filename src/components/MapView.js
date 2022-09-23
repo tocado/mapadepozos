3961827d44a07d87e3d8b99ce77274185f315a62
tocado <tocado.157@gmail.com>
2022-09-23 00:12:17 -0300

    chore: agrego la capa de cuencas reducida con topojson

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import React, { useState, useEffect } from "react";
+import { LayersControl, MapContainer, TileLayer } from "react-leaflet";
 import data from "../assets/srcSalta.json";
 import Markers from "./Markers";
-
+import { GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import * as topojson from "topojson-client";
 
 const MapView = (props) => {
+  const [cuencas, setCuencas] = useState({})
+
   const [state] = useState({
     currentLocation: { lat: -38.5094661, lng: -73.8996827 },
     zoom: 4,
@@ -14,23 +17,127 @@ const MapView = (props) => {
   let markers = data.map(function (o) {
     return {
       description: o.desc,
-      name: o.name,
+      name: <>
+        <h5>o.name</h5>
+        <table>
+          <tr>
+            <th>
+              Nombre
+            </th>
+            <td>
+              {o.name}
+            </td>
+          </tr>
+          <tr>
+            <th>
+              Caudal Medio
+            </th>
+            <td>
+              {o.Caudalmedio || '-'}
+            </td>
+          </tr>
+          <tr>
+            <th>
+              Profundiad
+            </th>
+            <td>
+              {o.Profundidad || '-'}
+            </td>
+          </tr>
+          <tr>
+            <th>
+              NivelDinamico
+            </th>
+            <td>
+              {o.NivelDinamico || '-'}
+            </td>
+          </tr>
+          <tr>
+            <th>
+              NivelEstatico
+            </th>
+            <td>
+              {o.NivelEstatico || '-'}
+            </td>
+          </tr>
+          <tr>
+            <th>
+              Uso
+            </th>
+            <td>
+              {o.Uso || '-'}
+            </td>
+          </tr>
+          <tr>
+            <th>
+              Provincia
+            </th>
+            <td>
+              {o.Provincia || '-'}
+            </td>
+          </tr>
+          <tr>
+            <th>
+              Fuente
+            </th>
+            <td>
+              {o.DuenioDelDato || '-'}
+            </td>
+          </tr>
+        </table>
+      </>,
       geometry: [
-        o.y.replace(',','.'),
-        o.x.replace(',','.')
+        o.y.replace(',', '.'),
+        o.x.replace(',', '.')
       ]
     }
   })
-  console.log(markers)
+
+  const onEach = (feature, layer) => {
+    let PopupContent = "<pre>" +
+      JSON.stringify(feature.properties, null, " ").replace(/[{}"]/g, "") +
+      "</pre>";
+    layer.bindPopup(PopupContent)
+  }
+  useEffect(() => {
+    let opt = {
+      method: 'GET',
+    }
+    // options = {
+    // method: 'POST',
+    // headers: headers,
+    // body: JSON.stringify({
+    // securityEnabledOnly: true,
+    // }),
+    fetch('http://localhost:3000/mapadepozos/cuencastopo.json', opt).then((res) => {
+      res.json().then((jsonCuencas) => {
+        debugger
+//        setCuencas(jsonCuencas)
+        setCuencas(topojson.feature(jsonCuencas,jsonCuencas.objects.cuencas))
+        document.getElementById('preloader').style.display = 'none'
+      })
+    })
+  }, [])
+
   return (
     <MapContainer center={state.currentLocation} zoom={state.zoom}>
       <TileLayer
         url="https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Markers markers={markers} />
+      <LayersControl position="topright">
+        <LayersControl.Overlay name="Pozos de Agua">
+          <Markers markers={markers} />
+        </LayersControl.Overlay>
+          <LayersControl.Overlay name="Cuencas de Agua">
+          {Object.keys(cuencas).length > 1 && (
+            <GeoJSON data={cuencas} onEachFeature={onEach} />
+          )}
+          </LayersControl.Overlay>
+      </LayersControl>
     </MapContainer>
   );
 };
-
+// comando para reducir
+// /c/Users/harkonnen/Documents/mapapozos/mapadepozos/node_modules/topojson/node_modules/topojson-server/bin/geo2topo -q 100000 src/components/cuencas.json >public/cuencastopo.json
 export default MapView;
--- src/components/Markers.js
+++ src/components/Markers.js
@@ -1,21 +1,33 @@
 import React from "react";
-import { LayerGroup, Circle, Tooltip } from "react-leaflet";
+import { Tooltip, Marker } from "react-leaflet";
 import MarkerPopup from "./MarkerPopup";
+import markerSVG from "../assets/marker.svg"
+import L from 'leaflet'
+import MarkerClusterGroup from '@changey/react-leaflet-markercluster'
+import 'react-leaflet-markercluster/dist/styles.min.css';
 
+const markerIcon = new L.Icon({
+  iconUrl: markerSVG,
+  iconRetinaUrl: markerSVG,
+  popupAnchor:  [-0, -0],
+  iconSize: [32,45],     
+});
 const Markers = (props) => {
   const { markers } = props;
   let TmpMarkers = <></>
   if (markers) {
     TmpMarkers = markers.map((m, i) => (
-      <LayerGroup>
-        <Circle key={i} center={m.geometry} pathOptions={{ color: 'red' }} radius={100}>
-          <MarkerPopup data={m} />
-          <Tooltip>{m.name}</Tooltip>
-        </Circle >
-      </LayerGroup>
+      <Marker key={i} position={m.geometry} icon={markerIcon}>
+        <MarkerPopup data={m} />
+        <Tooltip>{m.name}</Tooltip>
+      </Marker>
+      // <Circle key={i} center={m.geometry} pathOptions={{ color: 'red' }} radius={100}>
+      //   <MarkerPopup data={m} />
+      //   <Tooltip>{m.name}</Tooltip>
+      // </Circle >
     ));
   }
-  return <>{TmpMarkers}</>;
+  return <MarkerClusterGroup>{TmpMarkers}</MarkerClusterGroup>;
 };
 
 export default Markers;
