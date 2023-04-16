a710429fba05ce3b622723654785362e1715f4d8
tocado <tocado.157@gmail.com>
2023-04-16 20:44:25 -0300

    elimino el padding de la interfae

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -426,7 +426,7 @@ function Layout(props) {
             </Box>
             <Box
                 component="main"
-                sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+                sx={{ flexGrow: 1, p: 0, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
             >
                 <Toolbar />
                 <Grid container spacing={5} justifyContent="space-around">
